68ea7785e062911b5e53cd468ef2645640d8364d
endlessdev <ysw0094@gmail.com>
2017-05-10 10:27:28 +0900

    [Draft] Release hexo kawaii theme 1.0

--- themes/yeomme/source/scripts/common.js
+++ themes/yeomme/source/scripts/common.js
@@ -3,8 +3,11 @@ $(function () {
     var $commonHeader = $(".mobile-top-menu, .content-header");
     var $contentSection = $(".content-wrapper");
 
+    var $mobileSideNav = $(".main-nav-m");
+    var $navToggle = $(".ion-menu-icon, .ion-back-icon, .shadow");
+
     var lastScroll = 0;
-    $(document).scroll(function () {
+    $contentSection.scroll(function () {
         var scrollTopPotision = $(this).scrollTop();
         if (scrollTopPotision > lastScroll) {
             toggleHeader(true);
@@ -12,6 +15,18 @@ $(function () {
             toggleHeader(false);
         }
         lastScroll = scrollTopPotision;
+    });
+
+    $navToggle.click(function () {
+        if ($mobileSideNav.hasClass("slide-in")) {
+            $mobileSideNav.removeClass("slide-in");
+            $mobileSideNav.addClass("slide-out");
+        } else if ($mobileSideNav.hasClass("slide-out")) {
+            $mobileSideNav.removeClass("slide-out");
+            $mobileSideNav.addClass("slide-in");
+        } else {
+
+        }
     });
 
     function toggleHeader(isSlideIn) {
